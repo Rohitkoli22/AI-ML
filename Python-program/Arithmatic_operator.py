eae8b57f59b4de11ebc8a143262cbf0a2619b30b
Rohit <rohitskoli22@gmail.com>
2021-08-17 17:21:54 +0530

    Added operator operations

---
+++ Python-program/Arithmatic_operator.py
@@ -0,0 +1,21 @@
+a = 10
+b = 4
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a//b=",a//b)
+print("a%b=",a%b)
+print("a**b=",a**b)
+print("\n")
+print("Integer and Float value operations")
+b = 4.5
+
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a//b=",a//b)
+print("a%b=",a%b)
+print("a**b=",a**b)
+
---
+++ Python-program/Comparison_operator.py
@@ -0,0 +1,38 @@
+a = 10
+b = 5
+print("a>b :",a>b)
+print("a<b :",a<b)
+print("a==b :",a==b)
+print("a!=b :",a!=b)
+print("a>=b :",a>=b)
+print("a<=b :",a<=b)
+print("\n")
+print("Integer and Float value operations")
+b = 4.5
+print("a>b :",a>b)
+print("a<b :",a<b)
+print("a==b :",a==b)
+print("a!=b :",a!=b)
+print("a>=b :",a>=b)
+print("a<=b :",a<=b)
+print("\n")
+print("String operations")
+str = "abc"
+str1 = "ABC"
+print("str>str1 :",a>b)
+print("str<str1 :",a<b)
+print("str==bstr1 :",a==b)
+print("str!=str1 ",a!=b)
+print("str>=str1 :",a>=b)
+print("str<=str1 :",a<=b)
+print("\n")
+v1 = (1,2,3)
+v2 = (1,2,3,4)
+print("v1>v2 :",a>b)
+print("v1<v2 :",a<b)
+print("v1==v2 :",a==b)
+print("v1!=v2 :",a!=b)
+print("v1>=v2 :",a>=b)
+print("v1<=v2 :",a<=b)
+
+
